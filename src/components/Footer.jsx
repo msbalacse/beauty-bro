84d9a78f2eae2dfa6af8c5f3688c10480062b904
msbalacse <msbalacse@gmail.com>
2024-07-09 20:20:00 +0530

    updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const Footer = () => {
-  return (
-    <footer className="h-[400px] flex flex-col justify-center items-center bg-red-700 text-white">
-      <p>Footer</p>
-      <p>&copy; Beauty pro</p>
-    </footer>
-  );
+    return (
+        <footer className="h-[400px] flex flex-col justify-center items-center bg-red-700 text-white">
+            <p>Footer</p>
+            <p style={{ fontFamily: 'var(--font-butler)' }}>&copy; Beauty pro</p>
+        </footer>
+    );
 };
 
 export default Footer;
